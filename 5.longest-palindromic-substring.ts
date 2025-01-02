9791eb7958530057c01b19150a48695d7f21fb7d
Oguz Kazkayasi <oguz.kazkayasi@cleevio.com>
2025-01-02 10:34:22 +0300

    feat: Implement longest palindrome substring function
    - Added a helper function `checkIfPalindrome` to check if a given string is a palindrome.
    - Created another helper function `findLongest` to find the longest palindromic substring of a given length in the input string.
    - Updated the main function `longestPalindrome` to use these helper functions and return the longest palindromic substring.
    - Removed placeholder return statement from `longestPalindrome`.
    - Commented out previous implementation for reference.

--- 5.longest-palindromic-substring.ts
+++ 5.longest-palindromic-substring.ts
@@ -1,6 +1,61 @@
 // @leet start
+function checkIfPalindrome(s: string): boolean {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] === s[right]) {
+      left++;
+      right--;
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
+
+function findLongest(s: string, len: number): string | null {
+  let left = 0;
+  while (left + len <= s.length) {
+    const candidate = s.slice(left, left + len);
+    if (checkIfPalindrome(candidate)) {
+      return candidate;
+    }
+    left++;
+  }
+  return null;
+}
+
 function longestPalindrome(s: string): string {
-  return "";
-    
-};
+  let maxLen = s.length;
+  while (maxLen > 1) {
+    const longest = findLongest(s, maxLen);
+    if (longest != null) {
+      return longest;
+    } else {
+      maxLen--;
+    }
+  }
+  return s[0];
+
+  // let subString = "";
+  // let left = 0;
+
+  // let candidate = "";
+  // while (left < s.length) {
+  //   let right = left + 1;
+  //   while (right <= s.length) {
+  //     subString = s.slice(left, right);
+  //     if (subString.length > candidate.length) {
+  //       const isPalindrome = checkIfPalindrome(subString);
+  //       if (isPalindrome) {
+  //         candidate = subString;
+  //       }
+  //     }
+  //     right++;
+  //   }
+  //   left++;
+  // }
+  // return candidate;
+}
 // @leet end
