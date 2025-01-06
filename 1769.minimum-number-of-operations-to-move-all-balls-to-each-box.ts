5236c2700a67e5941282639571ff385f027fd772
Oguz Kazkayasi <oguz.kazkayasi@cleevio.com>
2025-01-06 12:26:59 +0300

    feat: Improve minOperations function in 1769.minimum-number-of-operations-to-move-all-balls-to-each-box.ts
    - Renamed original minOperations function to minOperationsBruteForce
    - Added a new optimized function, minOpsPrefixSum, which uses prefix sum method for calculation
    - Updated the main minOperations function to call the optimized version (minOpsPrefixSum) instead of brute force approach

--- 1769.minimum-number-of-operations-to-move-all-balls-to-each-box.ts
+++ 1769.minimum-number-of-operations-to-move-all-balls-to-each-box.ts
@@ -1,13 +1,41 @@
 // @leet start
-function minOperations(boxes: string): number[] {
 
+function minOpsPrefixSum(boxes: string): number[] {
+  const result: number[] = [];
+  let totalLeft = 0;
+  let totalRight = 0;
+  let countRight = 0;
+  let countLeft = 0;
+
+  // calculate totalRight for 0th index
+  for (let i = 1; i < boxes.length; i++) {
+    const boxIsFull = boxes[i] === "1";
+    countRight += boxIsFull ? 1 : 0;
+    totalRight += boxIsFull ? i : 0;
+    countLeft = boxes[0] === "1" ? 1 : 0;
+  }
+  result.push(totalRight);
+
+  for (let i = 1; i < boxes.length; i++) {
+    const boxIsFull = boxes[i] === "1";
+    totalRight = totalRight - countRight - (boxIsFull ? 1 : 0);
+    countRight = boxIsFull ? countRight - 1 : countRight;
+    totalLeft = totalLeft + countLeft + (boxIsFull ? 1 : 0);
+    countLeft = boxIsFull ? countLeft + 1 : countLeft;
+    result.push(totalRight + totalLeft);
+  }
+
+  return result;
+}
+
+function minOperationsBruteForce(boxes: string): number[] {
   const result: number[] = [];
   for (let i = 0; i < boxes.length; i++) {
     let total = 0;
 
     for (let j = 0; j < boxes.length; j++) {
       const box = boxes[j];
-      if (box === '1') {
+      if (box === "1") {
         total += Math.abs(i - j);
       }
     }
@@ -15,5 +43,9 @@ function minOperations(boxes: string): number[] {
     result.push(total);
   }
   return result;
+}
+
+function minOperations(boxes: string): number[] {
+  return minOpsPrefixSum(boxes);
 }
 // @leet end
