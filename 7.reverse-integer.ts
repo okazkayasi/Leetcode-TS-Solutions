d7ebc2f36d50362881381307ee9b6d9c824861c5
Oguz Kazkayasi <oguz.kazkayasi@cleevio.com>
2025-01-06 11:57:03 +0300

    feat: Enhance reverse integer function
    - Renamed original `reverse` function to `reverseAsString`.
    - Added new `reverseAsNumber` function that reverses an integer without converting it to a string.
    - Updated main `reverse` function to call the new `reverseAsNumber` method.

--- 7.reverse-integer.ts
+++ 7.reverse-integer.ts
@@ -1,5 +1,20 @@
 // @leet start
-function reverse(x: number): number {
+function reverseAsNumber(x: number): number {
+  const isNegative = x < 0;
+  let num = isNegative ? -1 * x : x;
+  let res = 0;
+
+  while (num > 0) {
+    const lastDigit = num % 10;
+    res = res * 10 + lastDigit;
+    num = Math.floor(num / 10);
+    if (res > Math.pow(2, 31) - 1) return 0;
+    if (res < -1 * Math.pow(2, 31)) return 0;
+  }
+  return isNegative ? -1 * res : res;
+}
+
+function reverseAsString(x: number): number {
   const isNegative = x < 0;
   const num = isNegative ? -1 * x : x;
 
@@ -12,5 +27,9 @@ function reverse(x: number): number {
   if (finalNum < -1 * Math.pow(2, 31)) return 0;
 
   return finalNum;
+}
+
+function reverse(x: number): number {
+  return reverseAsNumber(x);
 }
 // @leet end
